e7a632eaa09bc02242c04302b4650a58a8fa2414
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2017-05-26 16:11:06 +0200

    Added initial files

---
+++ ParallelTests.js
@@ -0,0 +1,56 @@
+var path = require('path'),
+	async = require('async'), //https://www.npmjs.com/package/async install by calling "npm install --save async"
+	newman = require('newman'), //https://github.com/postmanlabs/newman install by calling "npm install --save newman"
+
+	allowInsecureSSL = true, //allow insecure SSL certificates (I use this for local testing)
+	//newman.run takes two parameters: options and done.
+	//options is the collection run options, which include the file describing the collection and other stuff
+	//each collection that is being run may have its own options, different than the others
+	options = {
+		collection: path.join(__dirname, 'NewmanTests.postman_collection.json'),
+		reporters: 'cli',
+		insecure: allowInsecureSSL, //allow insecure SSL certificates (I use this for local testing)
+		globals: {
+			"values":[
+				{
+				"key": "aspAuthCookie", //The authentication cookie is set here for all calls
+                "value": "81FD75E54380206B27BDCA8C1BFDF1F509386D3EE16AA5403546471E078A3275201D7E23826741FEBDCD18A62C1B4DA1F01BFC4437FB27D320BA9F9A1C60F7854D7C449E2B22E655C0BFF5EE373C8F7F90338CEE1F7DD265D164640AAA03253AE43990C7116A25E7A6E2B879528DC491CBC6D5E6982E0F9D37A9CAF39BA25651",
+                "type": "text",
+                "enabled": true
+				}
+			]
+		}
+	},
+
+	//this is the collection runner function
+	parallelCollectionRun = function(done) {
+		newman.run(options, done); //done will be the callback function
+	},
+	//array of the collections to run
+	collections = [parallelCollectionRun], //initially only 1 element
+	//this will be called after all the concurrent calls are finished
+	recursiveCallBack = function (err, results) {
+		console.info("Done test for " + collections.length + " concurrent collections.");
+		err && console.error(err);
+		var fail;
+		if (results) {
+			results.forEach(function (result) {
+				fail = result.run.failures;
+				console.info(fail.length ? `${result.collection.name} failed.` :
+							`${result.collection.name} ran successfully.`);
+			});
+		} 
+		//keep increasing the number of concurrent calls until something fails
+		if ((fail && fail.length) || err) {
+			console.error("Error when attempting " + collections.length + " calls.");
+			err && console.error(err);
+		} else {
+			collections.push(parallelCollectionRun);
+			console.info("Launching test for " + collections.length + " concurrent collections.");
+			async.parallel(collections, recursiveCallBack);
+		}
+	};
+//--------------------------------------------------------------------------------------------//
+console.log("Start concurrency tests.");
+console.info("Launching test for " + collections.length + " concurrent collection.");
+async.parallel(collections, recursiveCallBack);
---
+++ README.md
@@ -0,0 +1,36 @@
+This script was written to allow simulating concurrent requests to tenant of [Orchard CMS](https://github.com/OrchardCMS/Orchard). Note that since JavaScript is single threaded, what this script really does is fire off calls in rapid succession, without waiting for one to end before firing off the next.
+
+## To run this script
+This script is supposed to be run using Node.js. Setup instructions are available [online](https://nodejs.org/en/#download).
+
+The script uses two modules:
+
+*[async.js](https://www.npmjs.com/package/async)
+*[newman](https://github.com/postmanlabs/newman)
+
+The easiest way to get the script running is to install both packages locally by running from command line:
+```
+$ npm install --save newman
+$ npm install --save async
+```
+
+After setup is complete, you can run 
+```
+$ node ParallelTests.js
+```
+The execution will stop as soon as one of the calls from the projection fails. At each iteration, the script will increase the number of concurrent collections it will run.
+
+## Customizing the script
+Before running the script, you should customize it for your needs.
+
+For authenticated calls, the first thing you should do is to input a valid authentication cookie string in the corresponding variable in the code. When setting up the calls for your collection, make sure to add the following header:
+```
+cookie:.ASPXAUTH={{aspAuthCookie}};
+```
+The aspAuthCookie variable is set by the script to the hardcoded value you input.
+
+You should verify that the collection name in the code corresponds to the name of your exported collection. The json collection file should be in the same folder that ParallelTests.js is in.
+The repository contains an example collection that we used locally on an ad-hoc instance.
+
+## License
+This software is licensed under Apache-2.0. Copyright Laser S.r.l.. See the [LICENSE](LICENSE) file for more information.
